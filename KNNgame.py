8ce1df54b3aeb42ffccc77e3d7a61527b87c193c
Morgan Sinclaire <sincm@uw.edu>
2017-02-21 18:15:49 -0800

    game is viable

---
+++ KNNgame.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def dist(a,b):
+    return ((a[0]-b[0])**2 + (a[1]-b[1])**2)**.5
+
+
+class Grid(object):
+    def __init__(self, size, blues=[], reds=[], k=1):
+        self.size = size
+        self.blues = blues
+        self.reds = reds
+
+    def add(self, point, color):
+        if color == "blue":
+            self.blues.append(point)
+        if color == "red":
+            self.reds.append(point)
+
+    def classify(self, point, k=1):
+        nearest = []
+        for p in self.blues:
+            nearest.append((dist(point,p), 0))
+        for p in self.reds:
+            nearest.append((dist(point,p), 1))
+
+        nearest = sorted(nearest)[:k]
+
+        count = sum([n[1] for n in nearest])
+
+        return count > k/2
+
+    def score(self, k=1, n=100):
+        count = 0
+
+        s = self.size
+        step = s / n
+
+        space = np.linspace(.5*s / n, s - .5*s / n, n)
+        for i in space:
+            for j in space:
+                count += self.classify((i,j), k)
+
+        pct = round(100*(count/n**2), 2)
+
+        if pct >= 50:
+            return "Red wins with {}%".format(pct)
+        else:
+            return "Blue wins with {}%".format(100 - pct)
+
+
+def game(n=100, size=10, k=1):
+    ply = int(input("Enter number of ply:\n"))
+
+    g = Grid(size=size, k=k)
+
+    for i in range(ply//2):
+        p = input("Red, place your point as x, y:\n")
+        p = tuple(map(float, p.replace(",", " ").split()))
+        g.reds.append(p)
+
+        p = input("Blue, place your point as x, y:\n")
+        p = tuple(map(float, p.replace(",", " ").split()))
+        g.blues.append(p)
+
+    return g.score(k, n)
+
+print(game())
